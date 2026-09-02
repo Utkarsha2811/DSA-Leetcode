02dad411d5a0b185064019a30784acee3a2c0577
Utkarsha Dunde <utkarshadunde@gmail.com>
2026-09-02 11:28:16 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,21 +4,21 @@ public:
         stack<char>st;
 
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                st.push(s[i]);
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){ // check if it is opening bracket 
+                st.push(s[i]); // push it into stack 
             }else{
-                if(st.empty()){
+                if(st.empty()){ // if a closing bracket comes and stack is empty it means no pair return false 
                     return false;
-                }
+                } // but if this condition satisfies check for the pairs 
                 if(st.top() == '(' && s[i] == ')' || st.top() == '{' && s[i] == '}' || st.top() == '[' && s[i] == ']'){
                     st.pop();
-                }else{
+                }else{ // and if this also not then return false 
                     return false;
                 }
             }
         }
 
-        return st.empty();
+        return st.empty(); // if the stack becomes empty atlast means each opening brakcet got its closing bracket and its valid else its false i.e, not valid 
 
     }
 };
